27ca59c3889bfd87ee07939967210e8c35e732d0
Guilherme Pereira <guiipereiraarts@gmail.com>
2025-09-09 20:15:43 -0300

    Delete navbar-responsiva-01 directory

--- navbar-responsiva-01/script.js
+++
@@ -1,8 +0,0 @@
-const openMenu = window.document.querySelector('#abrirMenu');
-const closeMenu = window.document.querySelector('#fecharMenu');
-
-openMenu.addEventListener('click', () => {
-    window.document.body.classList.toggle('mostrar-menu');
-});
-
-closeMenu.addEventListener('click', () => openMenu.click());
